223774e9127d938ee73a19f23a75cd1c204fb335
KieceDonc <valentinverst.developer@gmail.com>
2021-03-10 18:48:35 +0100

    XH

--- Exercice_H/XH04.c
+++ Exercice_H/XH04.c
@@ -11,6 +11,27 @@ typedef struct{
   Point b;
 }Segment;
 
-int main(){
+void print_point(Point* p){
+  printf("x = %f, y = %f\n",p->x,p->y);
+}
+
+void print_segment(Segment* s){
+  print_point(&s->a);
+  print_point(&s->b);
+}
+
+int main(){  
+  Segment s = {{0.0,0.0},{2.0,2.0}};
+  Segment tab0[2]; 
+  tab0[0] = s;
+  tab0[1] = s;
+  print_segment(&tab0[0]);
+  print_segment(&tab0[1]);
+  Segment* tab1 = (Segment*)malloc(2*sizeof(Segment));
+  tab1[0] = tab0[0];
+  tab1[1] = tab0[1];
+  print_segment(&tab1[0]);
+  print_segment(&tab1[1]);
+  free(tab1);
   return 0;
 }
---
+++ Exercice_H/XH06.cpp
@@ -0,0 +1,92 @@
+#include <stdio.h>
+
+class Point{
+  private:
+    double x;
+    double y;
+
+  public:
+    Point(double x, double y);
+
+    Point();
+
+    double getX();
+    double getY();
+    void setX(double x);
+    void setY(double y);
+
+    Point milieu(Point& autre);
+
+    void print();
+};
+
+Point::Point(double x, double y){
+  this->x = x;
+  this->y= y;
+};
+
+Point::Point(){
+  this->x = 0;
+  this->y= 0;
+};
+
+double Point::getX(){
+  return this->x;
+};
+
+double Point::getY(){
+  return this->y;
+};
+
+void Point::setX(double x){
+  this->x = x;
+};
+
+void Point::setY(double y){
+  this->y = y;
+};
+
+Point Point::milieu(Point& autre){
+  double Ptx=this->x; 
+  double Pty=this->y;
+  Ptx+=autre.x;
+  Pty+=autre.y;
+  Ptx=Ptx/2;
+  Pty=Pty/2;
+  return Point(Ptx,Pty);
+};
+
+void Point::print(){
+  printf("x = %f, y = %f\n",this->x,this->y);
+};
+
+class Segment{
+  private:
+    Point a;
+    Point b;
+
+  public:
+    Segment(double x1,double y1,double x2,double y2);
+
+    Segment();
+
+    Point milieu();
+
+    void print();
+};
+
+Segment::Segment(double x1,double y1,double x2,double y2) : a(x1,y1),b(x2,y2){
+  
+};
+
+void Segment::print(){
+  this->a.print();
+  this->b.print();
+}
+
+int main(){
+  Point* t = new Point(1,2);
+  Segment w = {1,2,1,2};
+  delete t;
+  return 0;
+}
