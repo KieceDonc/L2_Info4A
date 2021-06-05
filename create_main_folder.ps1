8deaa8425f9f06d1bd08c179cc59145e5c7b5733
KieceDonc <valentin.verstracte@gmail.com>
2021-06-05 15:53:54 +0200

    reorder

---
+++ create_main_folder.ps1
@@ -0,0 +1,10 @@
+﻿# powerhshell script to create main folder
+
+$main_folder_name = @("A","B","C","D","E","F","G","H","I");
+$path = "D:\Coding_WorkPlace\L2_Info4A\"; 
+
+cd $path;
+$main_folder_name.foreach{
+    MD "Partie_$PSItem";
+}
+
---
+++ reorder_script.ps1
@@ -0,0 +1,10 @@
+﻿# create folder for exercices files and move into them
+
+$path = "D:\Coding_WorkPlace\L2_Info4A\Partie_G\Exercices\"
+$name = "XG0"; 
+
+cd $path;
+1..6 | ForEach {
+MD "$name$_";
+Move-Item -Path ".\$name$_.cpp" -Destination ".\$name$_\$name$_.cpp";
+}
